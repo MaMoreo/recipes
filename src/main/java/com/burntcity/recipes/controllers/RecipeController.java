55cab77bf7076a41476f1a771736abca5c3e3b74
Miguel Moreo <miguelangel.moreo@gmail.com>
2020-11-03 15:19:06 +0100

    Added data.sql to fill DB

--- src/main/java/com/burntcity/recipes/controllers/RecipeController.java
+++ src/main/java/com/burntcity/recipes/controllers/RecipeController.java
@@ -1,13 +1,38 @@
 package com.burntcity.recipes.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.burntcity.recipes.domain.Category;
+import com.burntcity.recipes.domain.UnitOfMeasure;
+import com.burntcity.recipes.repositories.CategoryRepository;
+import com.burntcity.recipes.repositories.UnitOfMeasureRepository;
 
 @Controller
 public class RecipeController {
 
+	private final CategoryRepository categoryRepository;
+	private final UnitOfMeasureRepository unitOfMeasureRepository;
+	
+	
+	
+	public RecipeController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+		super();
+		this.categoryRepository = categoryRepository;
+		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	}
+
 	@RequestMapping({"", "/", "index", "index.html"})
 	public String getIndex() {
+		
+		Optional<Category> optionalCat = categoryRepository.findByDescription("American");
+		Optional<UnitOfMeasure> optionalUnitOfMes = unitOfMeasureRepository.findByDescription("Tablespoon");
+		
+		System.out.println("Cat ID is " + optionalCat.get().getId());
+		System.out.println("UOM ID is " + optionalUnitOfMes.get().getId());
+		
 		return "index";
 	}
 	
--- src/main/java/com/burntcity/recipes/repositories/CategoryRepository.java
+++ src/main/java/com/burntcity/recipes/repositories/CategoryRepository.java
@@ -1,9 +1,13 @@
 package com.burntcity.recipes.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.burntcity.recipes.domain.Category;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+	
+	Optional<Category> findByDescription(String description);
 
 }
